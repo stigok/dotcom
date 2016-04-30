d58010a9114965fd90f58ab221c87cc94cc09ff2
stigok <stig@stigok.com>
2016-04-30 14:36:36 +0000

    Configure showdown options

--- app/app.js
+++ app/app.js
@@ -13,16 +13,16 @@ angular.module('snippetshow', [
 
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-    .when('/posts/:category/:id', {
-      templateUrl: 'views/posts/postDetails.html',
-      controller: 'PostDetailViewController'
-    })
-    .when('/posts/:category?', {
-      templateUrl: 'views/posts/posts.html',
+    .when('/posts', {
       controller: 'PostsController'
     })
     .when('/about', {
       templateUrl: 'templates/about.html'
     })
     .otherwise({redirectTo: '/posts'});
-}]);
+}])
+
+.config(function ($showdownProvider) {
+  $showdownProvider.setOption('noHeaderId', true);
+  $showdownProvider.setOption('headerLevelStart', 2);
+});
