9e99e9551caa95d9be4cea1cd744850ad7c4094a
stigok <stig@stigok.com>
2016-05-06 01:33:52 +0000

    Static post loading

--- app/components/data-source/service.js
+++ app/components/data-source/service.js
@@ -5,29 +5,17 @@ angular.module('snippetshow.components.posts', [])
 .service('Posts', ['$http', '$q', function ($http, $q) {
   return {
     query: function () {
-      return $q(function (resolve) {
-        resolve([
-          {
-            type: 'text',
-            title: 'Hello, world!',
-            body: 'What is up?',
-            tags: ['hello', 'stuff', 'weed sessions']
-          }
-        ]);
-      });/*
-      return $http.get('@@apiEndpoint/posts').then(function (response) {
+      //return $http.get('@@apiEndpoint/posts').then(function (response) {
+      return $http.get('/api/posts.json').then(function (response) {
         return response.data;
-      });*/
+      });
     },
-    get: function (postid) {
-      return $http.get('/api/singlepost.json',
-        {
-          params: {
-            api_key: 'JaxGeeKKJw1taQn4M0WhuaYgsYcuxIp0TSMcaYmZ4JdelPU4Yd',
-            id: postid
-          }
-        }).then(function (response) {
-          return response.data.response.posts[0];
+    get: function (id) {
+      return $http.get('/api/posts.json')
+        .then(function (response) {
+          var posts = response.data;
+          var single = _.findWhere(posts, {id: Number(id)});
+          return single;
         });
     }
   };
